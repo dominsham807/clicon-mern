0b1400a1ac9b00cc2fa2032f3d8940edbb922414
Dominic Sham <dominicsham@cms.hkcable.com>
2024-05-21 17:17:58 +0800

    Add test route

--- clicon-server/routes/authRoute.js
+++ clicon-server/routes/authRoute.js
@@ -4,6 +4,10 @@ import { authenticateUser } from "../middlewares/authentication.js"
 
 const router = express.Router()
 
+router.get("/test", (req,res) => {
+    res.send("Test Auth route");
+})
+
 router.post("/register", registerUser)
 router.post("/verify", verifyEmail)
 router.post("/login", loginUser)
