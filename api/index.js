ad1b9373660dc592b925f54bb771948170ae5868
Dominic Sham <dominicsham@cms.hkcable.com>
2024-05-23 10:10:37 +0800

    Changed build route

--- api/index.js
+++ api/index.js
@@ -42,11 +42,10 @@ console.log(__dirname)
 
 // app.use(express.static("public"));
 
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+if (process.env.NODE_ENV === "production") { 
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'),function (err) {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
             if(err) {
                 res.status(500).send(err)
             }
